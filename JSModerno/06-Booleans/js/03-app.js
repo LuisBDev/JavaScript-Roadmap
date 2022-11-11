b3c7c05d0e241e92a4c78407c11c69f044c5ccaa
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2022-11-11 11:23:42 -0500

    booleans

---
+++ JSModerno/06-Booleans/js/03-app.js
@@ -0,0 +1,13 @@
+const autenticado = true;
+
+if (autenticado) {
+    console.log("message");
+}
+else {
+    console.log("error");
+}
+
+//operador ternario
+
+console.log(autenticado ? 'Si autenticado' : 'No autenticado');
+
